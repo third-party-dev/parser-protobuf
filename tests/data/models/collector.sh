ae4df11c67b51f375c78af341a8547f072e8cd8f
chenz <crazychenz@gmail.com>
2025-11-24 09:48:42 +0000

    Added collector

---
+++ tests/data/models/collector.sh
@@ -0,0 +1,33 @@
+
+
+
+# GPT2
+wget https://huggingface.co/openai-community/gpt2/resolve/main/onnx/decoder_model.onnx
+wget https://huggingface.co/openai-community/gpt2/resolve/main/pytorch_model.bin
+wget https://huggingface.co/openai-community/gpt2/resolve/main/64.tflite
+wget https://huggingface.co/openai-community/gpt2/resolve/main/model.safetensors
+
+
+# Whisper
+
+## PyTorch Config
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/config.json
+
+## Safetensors Shards
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/model.safetensors.index.fp32.json
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/model.fp32-00001-of-00002.safetensors
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/model.fp32-00002-of-00002.safetensors
+
+## Safetensor Whole?
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/model.safetensors
+
+## PyTorch Shards
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/pytorch_model.bin.index.fp32.json
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/pytorch_model.bin.index.fp32.json
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/pytorch_model.fp32-00002-of-00002.bin
+
+## PyTorch Whole?
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/pytorch_model.bin
+
+## MsgPack
+wget https://huggingface.co/openai/whisper-large-v3/resolve/main/flax_model.msgpack
